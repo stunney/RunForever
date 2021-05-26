688383f0bfa8250518aef13479df08fb3f9f87db
S. Tunney <stephen.tunney@gmail.com>
2021-05-26 11:51:31 -0400

    The one and only file
    calls "start.bat".  Can call anything really.  Cross platform if you want it to be.

---
+++ AlwaysRun.ps1
@@ -0,0 +1,6 @@
+﻿while(1) {
+    $process = Start-Process -FilePath "start.bat" -WindowStyle Minimized -PassThru
+    $process.WaitForExit()
+    echo "Process closed.  Waiting to open again"
+    Sleep -Seconds 60
+};
